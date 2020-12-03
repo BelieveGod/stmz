3eff3e0885183fdb6d3dcec504f366ed03473477
LTJ <597432232@qq.com>
2020-12-03 11:55:51 +0800

    一个看似成功的最小覆盖圆算法

--- src/main/java/com/stmz/mgr/test/CircleTest.java
+++ src/main/java/com/stmz/mgr/test/CircleTest.java
@@ -10,35 +10,72 @@ import org.junit.jupiter.api.Test;
 public class CircleTest {
 
 //  半径
-    private Double R;
+    private Double r;
 // 圆心
-    private Point O;
-
-    private Point[] points=new Point[]{new Point(0d,0d),new Point(0d,2d),new Point(1d,1d)};
+    private Point d=new Point();
+// 数组
+//    private Point[] a=new Point[]{new Point(0d,0d),new Point(0d,2d),new Point(1d,1d),new Point(1.7d,1.4d)};
+    private Point[] a=new Point[]{new Point(0d,0d),new Point(0d,4d),new Point(8d,4d)};
     // 两点距离
-    private Double distance(Point a,Point b){
-        return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+    private Double distance(Point p1,Point p2){
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
     }
 
     private Double sqr(Double x){
         return x*x;
     }
 
-    private Double step(Point a,Point b){
-       return  (sqr(b.x)+sqr(b.y)-sqr(a.x)-sqr(a.y))/2;
+
+
+
+
+    Double multiply(Point p1,Point p2,Point p0){
+        return   ((p1.x-p0.x)*(p2.y-p0.y)-(p2.x-p0.x)*(p1.y-p0.y));
     }
 
-    boolean inCircle(Point a){
-        if(distance(a,O).compareTo(R)<=0){
-            return true;
+    void MiniDiscWith2Point(Point   p,Point   q,int   n){
+        d.x=(p.x+q.x)/2.0;
+        d.y=(p.y+q.y)/2.0;
+        r=distance(p,q)/2;
+        int k;
+        double c1,c2,t1,t2,t3;
+        for(k=0;k<n;k++){
+            if(distance(d,a[k])<=r)continue;
+            if(multiply(p,q,a[k])!=0.0){
+                c1=(p.x*p.x+p.y*p.y-q.x*q.x-q.y*q.y)/2.0;
+                c2=(p.x*p.x+p.y*p.y-a[k].x*a[k].x-a[k].y*a[k].y)/2.0;
+                d.x=(c1*(p.y-a[k].y)-c2*(p.y-q.y))/((p.x-q.x)*(p.y-a[k].y)-(p.x-a[k].x)*(p.y-q.y));
+                d.y=(c1*(p.x-a[k].x)-c2*(p.x-q.x))/((p.y-q.y)*(p.x-a[k].x)-(p.y-a[k].y)*(p.x-q.x));
+                r=distance(d,a[k]);
+            }
+            else{
+                t1=distance(p,q);
+                t2=distance(q,a[k]);
+                t3=distance(p,a[k]);
+                if(t1>=t2&&t1>=t3){
+                    d.x=(p.x+q.x)/2.0;d.y=(p.y+q.y)/2.0;r=distance(p,q)/2.0;
+                }
+                else if(t2>=t1&&t2>=t3){
+                    d.x=(a[k].x+q.x)/2.0;d.y=(a[k].y+q.y)/2.0;r=distance(a[k],q)/2.0;
+                }
+                else{
+                    d.x=(a[k].x+p.x)/2.0;d.y=(a[k].y+p.y)/2.0;r=distance(a[k],p)/2.0;
+                }
+            }
         }
-        return false;
     }
 
-    private Point solve(Double a,Double b,Double c,Double d,Double e,Double f){
-        Double y=(f*a-c*d)/(b*d-e*a);
-        Double x=(f*b-c*e)/(a*e-b*d);
-        return new Point(x,y);
+    void MiniDiscWithPoint(Point   pi,int   n){
+        d.x=(pi.x+a[0].x)/2.0;
+        d.y=(pi.y+a[0].y)/2.0;
+        r=distance(pi,a[0])/2.0;
+        int j;
+        for(j=1;j<n;j++){
+            if(distance(d,a[j])<=r)continue;
+            else{
+                MiniDiscWith2Point(pi,a[j],j-1);
+            }
+        }
     }
 
     @Test
@@ -51,34 +88,17 @@ public class CircleTest {
 
     @Test
     public void test2(){
-        int n=points.length;
-        R=0d;
-        O=points[0];
-        for(int i=1;i<n;i++){
-            if(!inCircle(points[i])){
-                O.x=points[i].x;
-                O.y=points[i].y;
-                R=0d;
-                for(int j=0;j<i;j++){
-                    if(!inCircle(points[j])){
-                        O.x=(points[i].x+points[j].x)/2;
-                        O.y=(points[i].y+points[j].y)/2;
-                        R = distance(O, points[i]);
-                        for(int k=0;k<j;k++){
-                            if(!inCircle(points[k])){
-                                O=solve(points[i].x-points[j].x,points[i].y-points[j].y,step(points[i],points[j]),
-                                        points[i].x-points[k].x,points[i].y-points[k].y,step(points[i],points[k])
-                                );
-                                R=distance(points[i],O);
-                            }
-                        }
-
-                    }
-                }
+        int n=a.length;
+        r=distance(a[0],a[1])/2.0;
+        d.x=(a[0].x+a[1].x)/2.0;
+        d.y=(a[0].y+a[1].y)/2.0;
+        for(int i=2;i<n;i++){
+            if(distance(d,a[i])<=r)continue;
+            else{
+                MiniDiscWithPoint(a[i],i-1);
             }
         }
-        System.out.println("("+O.x+","+O.y+")");
-        System.out.println("R = " + R);
+        System.out.println(d.x+","+d.y+" R="+r);
     }
 
 
